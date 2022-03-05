56fa33086fecf3bdb7d93a1650e10d956a8e54a6
Aleks Petrov <aleks0317p@gmail.com>
2022-03-05 22:03:34 +0100

    Функция добавления данных в БД и получение их из БД

---
+++ Main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type tableUser struct {
+	name     string
+	lastName string
+	login    string
+	password string
+	deleted  string
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:Lozin3992ka@/testbd")
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	result, err := db.Exec("insert into testbd.user (name, lastName, login, password, deleted) values ('Aleks', 'Piatrou', 'PetAL', '12345', 'No')")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result.RowsAffected())
+
+	rows, err := db.Query("select * from testbd.user")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	users := []tableUser{}
+
+	for rows.Next() {
+		p := tableUser{}
+		err := rows.Scan(&p.name, &p.lastName, &p.login, &p.password, &p.deleted)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		users = append(users, p)
+	}
+	for _, p := range users {
+		fmt.Println(p.name, p.lastName, p.login, p.password, p.deleted)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module Authorization
+
+go 1.17
+
+require (
+	gorm.io/driver/sqlite v1.3.1
+	gorm.io/gorm v1.23.2
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/mattn/go-sqlite3 v1.14.9 // indirect
+)
